6b0b50e6ef3995cadc28313488e394295d01b259
PrjKomal <110466516+PrjKomal@users.noreply.github.com>
2023-05-19 13:24:55 +0530

    creating two page for app

--- static/jira-estimate/src/App.js
+++ static/jira-estimate/src/App.js
@@ -1,16 +1,57 @@
-import React, { useEffect, useState } from 'react';
-import { invoke } from '@forge/bridge';
+import React, { useEffect, useState } from "react";
+import { BrowserRouter, Routes, Route, Router } from "react-router-dom";
+import { view } from "@forge/bridge";
+import HomePage from "./Components/DashBoard/HomePage";
+import ResourcesPage from "./Components/AnotherPage/ResourcesPage";
+
 
 function App() {
-    const [data, setData] = useState(null);
+    const [history, setHistory] = useState(null);
 
     useEffect(() => {
-        invoke('getText', { example: 'my-invoke-variable' }).then(setData);
+        view.createHistory().then((newHistory) => {
+            setHistory(newHistory);
+        });
     }, []);
+
+    const [historyState, setHistoryState] = useState(null);
+
+    useEffect(() => {
+        if (!historyState && history) {
+            setHistoryState({
+                action: history.action,
+                location: history.location,
+            });
+        }
+    }, [history, historyState]);
+
+    useEffect(() => {
+        if (history) {
+            history.listen((location, action) => {
+                setHistoryState({
+                    action,
+                    location,
+                });
+            });
+        }
+    }, [history]);
 
     return (
         <div>
-            {data ? data : 'Loading...'}
+            {history && historyState ? (
+                <Router
+                    navigator={history}
+                    navigationType={historyState.action}
+                    location={historyState.location}
+                >
+                    <Routes>
+                        <Route path="/dashboard" element={<HomePage />}></Route>
+                        <Route path="/another-page" element={<ResourcesPage />}></Route>
+                    </Routes>
+                </Router>
+            ) : (
+                "Loading..."
+            )}
         </div>
     );
 }
