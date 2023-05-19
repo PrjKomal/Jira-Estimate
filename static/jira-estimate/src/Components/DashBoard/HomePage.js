016489b5c99634499f891d0a09624f94e4e8c239
PrjKomal <110466516+PrjKomal@users.noreply.github.com>
2023-05-19 13:42:13 +0530

    Update HomePage.js

--- static/jira-estimate/src/Components/DashBoard/HomePage.js
+++ static/jira-estimate/src/Components/DashBoard/HomePage.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import styles from './styles.module.scss'
-import Filter from '../HOC/Filter/Filter'
+import Filter from '../HOC/Filter/FIlter'
 
 const HomePage = () => {
     return (
