f6bf98827cb47d041867931753d8319fc183fd94
PrjKomal <110466516+PrjKomal@users.noreply.github.com>
2023-05-22 11:13:16 +0530

    react beautiful dnd added

--- static/jira-estimate/src/index.js
+++ static/jira-estimate/src/index.js
@@ -2,15 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import "./index.css";
-import { DndProvider } from 'react-dnd'
-import Backend from 'react-dnd-html5-backend'
 import '@atlaskit/css-reset';
 
 ReactDOM.render(
     <React.StrictMode>
-        <DndProvider backend={Backend}>
             <App />
-        </DndProvider>
     </React.StrictMode>,
     document.getElementById('root')
 );
