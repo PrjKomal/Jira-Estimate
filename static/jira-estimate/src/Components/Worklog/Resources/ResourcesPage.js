2136b7437e33905fdd2edaf150f58c83add55734
PrjKomal <110466516+PrjKomal@users.noreply.github.com>
2023-06-05 14:36:51 +0530

    update ui

--- static/jira-estimate/src/Components/Worklog/Resources/ResourcesPage.js
+++ static/jira-estimate/src/Components/Worklog/Resources/ResourcesPage.js
@@ -1,113 +1,123 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import styles from './styles.module.scss';
 import { v4 as uuidv4 } from 'uuid';
 import rightArrowIcon from '../../../assests/images/Icon awesome-caret-down.svg';
 import downArrowIcon from '../../../assests/images/Icon awesome-caret-down (1).svg';
+import { invoke } from '@forge/bridge'
 
 const ResourcesPage = () => {
-    const allIssues = [
-        {
-            id: '10014',
-            key: 'TEST-11',
-            summary: 'Another task',
-            iconUrl: 'https://first-forge-app.atlassian.net/rest/api/2/universal_avatar/view/type/issuetype/avatar/10318?size=medium',
-            description: null,
-            assignee: {},
-            project: { project_id: '10001', project_key: 'TEST' },
-            priorityUrl: 'https://first-forge-app.atlassian.net/images/icons/priorities/medium.svg',
-            status: 'To Do',
-            startDate: null,
-            duedate: undefined,
-            actualTime: null
-        },
-        {
-            id: '10013',
-            key: 'TEST-10',
-            summary: 'Test board 1st task created',
-            iconUrl: 'https://first-forge-app.atlassian.net/rest/api/2/universal_avatar/view/type/issuetype/avatar/10318?size=medium',
-            description: null,
-            assignee: {
-                accountId: '712020:ca1d6a91-f96f-4975-81de-a91797e08192',
-                assigneeUrl: 'https://secure.gravatar.com/avatar/001b105e386299ed86f919f870c44cae?d=https%3A%2F%2Favatar-management--avatars.us-west-2.prod.public.atl-paas.net%2Finitials%2FK-0.png',
-                displayName: 'Komal'
-            },
-            project: { project_id: '10001', project_key: 'TEST' },
-            priorityUrl: 'https://first-forge-app.atlassian.net/images/icons/priorities/medium.svg',
-            status: 'To Do',
-            startDate: '2023-05-17',
-            duedate: undefined,
-            actualTime: "30m"
-        },
-        {
-            id: '10016',
-            key: 'KAN-5',
-            summary: 'VHJ',
-            iconUrl: 'https://first-forge-app.atlassian.net/rest/api/2/universal_avatar/view/type/issuetype/avatar/10307?size=medium',
-            description: null,
-            assignee: {},
-            project: { project_id: '10000', project_key: 'KAN' },
-            priorityUrl: 'https://first-forge-app.atlassian.net/images/icons/priorities/medium.svg',
-            status: 'To Do',
-            startDate: '2023-05-18',
-            duedate: undefined,
-            actualTime: null
-        },
-        {
-            id: '10003',
-            key: 'KAN-4',
-            summary: 'Test Bug',
-            iconUrl: 'https://first-forge-app.atlassian.net/rest/api/2/universal_avatar/view/type/issuetype/avatar/10303?size=medium',
-            description: null,
-            assignee: {
-                accountId: '640eee615534b0bf743f5753',
-                assigneeUrl: 'https://secure.gravatar.com/avatar/8ccef5c0f874eb307a477454dfd0a13e?d=https%3A%2F%2Favatar-management--avatars.us-west-2.prod.public.atl-paas.net%2Finitials%2FKK-3.png',
-                displayName: 'Khushi Kumari'
-            },
-            project: { project_id: '10000', project_key: 'KAN' },
-            priorityUrl: 'https://first-forge-app.atlassian.net/images/icons/priorities/medium.svg',
-            status: 'Done',
-            startDate: '2023-05-30',
-            duedate: undefined,
-            actualTime: "30m"
-        },
-        {
-            id: '10001',
-            key: 'KAN-2',
-            summary: 'Test for another issue',
-            iconUrl: 'https://first-forge-app.atlassian.net/rest/api/2/universal_avatar/view/type/issuetype/avatar/10318?size=medium',
-            description: null,
-            assignee: {
-                accountId: '712020:ca1d6a91-f96f-4975-81de-a91797e08192',
-                assigneeUrl: 'https://secure.gravatar.com/avatar/001b105e386299ed86f919f870c44cae?d=https%3A%2F%2Favatar-management--avatars.us-west-2.prod.public.atl-paas.net%2Finitials%2FK-0.png',
-                displayName: 'Komal'
-            },
-            project: { project_id: '10000', project_key: 'KAN' },
-            priorityUrl: 'https://first-forge-app.atlassian.net/images/icons/priorities/medium.svg',
-            status: 'To Do',
-            startDate: '2023-05-18',
-            duedate: undefined,
-            actualTime: "30m"
-        },
-        {
-            id: '10000',
-            key: 'KAN-1',
-            summary: 'Task for fetch issue',
-            iconUrl: 'https://first-forge-app.atlassian.net/rest/api/2/universal_avatar/view/type/issuetype/avatar/10318?size=medium',
-            description: null,
-            assignee: {
-                accountId: '712020:ca1d6a91-f96f-4975-81de-a91797e08192',
-                assigneeUrl: 'https://secure.gravatar.com/avatar/001b105e386299ed86f919f870c44cae?d=https%3A%2F%2Favatar-management--avatars.us-west-2.prod.public.atl-paas.net%2Finitials%2FK-0.png',
-                displayName: 'Komal'
-            },
-            project: { project_id: '10000', project_key: 'KAN' },
-            priorityUrl: 'https://first-forge-app.atlassian.net/images/icons/priorities/medium.svg',
-            status: 'In Progress',
-            startDate: '2023-05-17',
-            duedate: undefined,
-            actualTime: "30m"
-        }
-    ]
+    // const allIssues = [
+    //     {
+    //         id: '10014',
+    //         key: 'TEST-11',
+    //         summary: 'Another task',
+    //         iconUrl: 'https://first-forge-app.atlassian.net/rest/api/2/universal_avatar/view/type/issuetype/avatar/10318?size=medium',
+    //         description: null,
+    //         assignee: {},
+    //         project: { project_id: '10001', project_key: 'TEST' },
+    //         priorityUrl: 'https://first-forge-app.atlassian.net/images/icons/priorities/medium.svg',
+    //         status: 'To Do',
+    //         startDate: null,
+    //         duedate: undefined,
+    //         actualTime: null
+    //     },
+    //     {
+    //         id: '10013',
+    //         key: 'TEST-10',
+    //         summary: 'Test board 1st task created',
+    //         iconUrl: 'https://first-forge-app.atlassian.net/rest/api/2/universal_avatar/view/type/issuetype/avatar/10318?size=medium',
+    //         description: null,
+    //         assignee: {
+    //             accountId: '712020:ca1d6a91-f96f-4975-81de-a91797e08192',
+    //             assigneeUrl: 'https://secure.gravatar.com/avatar/001b105e386299ed86f919f870c44cae?d=https%3A%2F%2Favatar-management--avatars.us-west-2.prod.public.atl-paas.net%2Finitials%2FK-0.png',
+    //             displayName: 'Komal'
+    //         },
+    //         project: { project_id: '10001', project_key: 'TEST' },
+    //         priorityUrl: 'https://first-forge-app.atlassian.net/images/icons/priorities/medium.svg',
+    //         status: 'To Do',
+    //         startDate: '2023-05-17',
+    //         duedate: undefined,
+    //         actualTime: "30m"
+    //     },
+    //     {
+    //         id: '10016',
+    //         key: 'KAN-5',
+    //         summary: 'VHJ',
+    //         iconUrl: 'https://first-forge-app.atlassian.net/rest/api/2/universal_avatar/view/type/issuetype/avatar/10307?size=medium',
+    //         description: null,
+    //         assignee: {},
+    //         project: { project_id: '10000', project_key: 'KAN' },
+    //         priorityUrl: 'https://first-forge-app.atlassian.net/images/icons/priorities/medium.svg',
+    //         status: 'To Do',
+    //         startDate: '2023-05-18',
+    //         duedate: undefined,
+    //         actualTime: null
+    //     },
+    //     {
+    //         id: '10003',
+    //         key: 'KAN-4',
+    //         summary: 'Test Bug',
+    //         iconUrl: 'https://first-forge-app.atlassian.net/rest/api/2/universal_avatar/view/type/issuetype/avatar/10303?size=medium',
+    //         description: null,
+    //         assignee: {
+    //             accountId: '640eee615534b0bf743f5753',
+    //             assigneeUrl: 'https://secure.gravatar.com/avatar/8ccef5c0f874eb307a477454dfd0a13e?d=https%3A%2F%2Favatar-management--avatars.us-west-2.prod.public.atl-paas.net%2Finitials%2FKK-3.png',
+    //             displayName: 'Khushi Kumari'
+    //         },
+    //         project: { project_id: '10000', project_key: 'KAN' },
+    //         priorityUrl: 'https://first-forge-app.atlassian.net/images/icons/priorities/medium.svg',
+    //         status: 'Done',
+    //         startDate: '2023-05-30',
+    //         duedate: undefined,
+    //         actualTime: "30m"
+    //     },
+    //     {
+    //         id: '10001',
+    //         key: 'KAN-2',
+    //         summary: 'Test for another issue',
+    //         iconUrl: 'https://first-forge-app.atlassian.net/rest/api/2/universal_avatar/view/type/issuetype/avatar/10318?size=medium',
+    //         description: null,
+    //         assignee: {
+    //             accountId: '712020:ca1d6a91-f96f-4975-81de-a91797e08192',
+    //             assigneeUrl: 'https://secure.gravatar.com/avatar/001b105e386299ed86f919f870c44cae?d=https%3A%2F%2Favatar-management--avatars.us-west-2.prod.public.atl-paas.net%2Finitials%2FK-0.png',
+    //             displayName: 'Komal'
+    //         },
+    //         project: { project_id: '10000', project_key: 'KAN' },
+    //         priorityUrl: 'https://first-forge-app.atlassian.net/images/icons/priorities/medium.svg',
+    //         status: 'To Do',
+    //         startDate: '2023-05-18',
+    //         duedate: undefined,
+    //         actualTime: "30m"
+    //     },
+    //     {
+    //         id: '10000',
+    //         key: 'KAN-1',
+    //         summary: 'Task for fetch issue',
+    //         iconUrl: 'https://first-forge-app.atlassian.net/rest/api/2/universal_avatar/view/type/issuetype/avatar/10318?size=medium',
+    //         description: null,
+    //         assignee: {
+    //             accountId: '712020:ca1d6a91-f96f-4975-81de-a91797e08192',
+    //             assigneeUrl: 'https://secure.gravatar.com/avatar/001b105e386299ed86f919f870c44cae?d=https%3A%2F%2Favatar-management--avatars.us-west-2.prod.public.atl-paas.net%2Finitials%2FK-0.png',
+    //             displayName: 'Komal'
+    //         },
+    //         project: { project_id: '10000', project_key: 'KAN' },
+    //         priorityUrl: 'https://first-forge-app.atlassian.net/images/icons/priorities/medium.svg',
+    //         status: 'In Progress',
+    //         startDate: '2023-05-17',
+    //         duedate: undefined,
+    //         actualTime: "30m"
+    //     }
+    // ]
 
+    const [allIssues, setAllIssues] = useState([])
+    useEffect(() => {
+        (async () => {
+            // Can be done using resolvers
+            // TO get all issues 
+            const data = await invoke('getAllIssues', { project_name: "" });
+            setAllIssues(data)
+        })();
+    }, []);
 
     // Get unique weekdays
     const weekdays = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday'];
